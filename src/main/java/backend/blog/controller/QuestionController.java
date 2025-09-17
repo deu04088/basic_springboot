215f6021eca622fae4c3f778643233473b9dc52b
park <deu04088@naver.com>
2025-09-17 15:45:29 +0900

    ADD: 2_8~9Unit complete

--- src/main/java/backend/blog/controller/QuestionController.java
+++ src/main/java/backend/blog/controller/QuestionController.java
@@ -2,6 +2,7 @@ package backend.blog.controller;
 
 import backend.blog.domain.Question;
 import backend.blog.repository.QuestionRepository;
+import backend.blog.service.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,12 +14,12 @@ import java.util.List;
 @RequiredArgsConstructor                            // final이 붙은 속성을 포함하는 생성자를 자동으로 생성
 @Controller
 public class QuestionController {
-    private final QuestionRepository questionRepository;
+    private final QuestionService questionService;
     
     @GetMapping("/question/list")
 //    @ResponseBody
     public String list(Model model) {               // 매개 변수 Model이 객체를 자동 생성
-        List<Question> questionList = this.questionRepository.findAll();
+        List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);       // questionList 데이터를 questionList라는 이름으로 model 객체에 저장
         return "question_list";
     }
---
+++ src/main/java/backend/blog/service/QuestionService.java
@@ -0,0 +1,18 @@
+package backend.blog.service;
+
+import backend.blog.domain.Question;
+import backend.blog.repository.QuestionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class QuestionService {
+    private final QuestionRepository questionRepository;
+
+    public List<Question> getList() {
+        return this.questionRepository.findAll();
+    }
+}
