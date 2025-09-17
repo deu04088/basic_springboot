3dddc3b719ae5d174bba38b542e75e659e3a02d4
park <deu04088@naver.com>
2025-09-17 16:28:51 +0900

    ADD: 2_10Unit complete

--- src/main/java/backend/blog/controller/QuestionController.java
+++ src/main/java/backend/blog/controller/QuestionController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -22,5 +23,12 @@ public class QuestionController {
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);       // questionList 데이터를 questionList라는 이름으로 model 객체에 저장
         return "question_list";
+    }
+
+    @GetMapping(value = "/question/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
     }
 }
--- src/main/java/backend/blog/service/QuestionService.java
+++ src/main/java/backend/blog/service/QuestionService.java
@@ -1,11 +1,13 @@
 package backend.blog.service;
 
+import backend.blog.DataNotFoundException;
 import backend.blog.domain.Question;
 import backend.blog.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -14,5 +16,15 @@ public class QuestionService {
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();                          // Optional이 값을 가지고 있을 때 반환
+        }
+        else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
