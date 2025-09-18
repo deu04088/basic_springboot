16f255febf908d89210f7c0b8eba41d64e7c9929
park <deu04088@naver.com>
2025-09-18 15:59:29 +0900

    ADD: 2_11Unit complete

--- src/main/java/backend/blog/controller/QuestionController.java
+++ src/main/java/backend/blog/controller/QuestionController.java
@@ -8,16 +8,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
+@RequestMapping("/question")
 @RequiredArgsConstructor                            // final이 붙은 속성을 포함하는 생성자를 자동으로 생성
 @Controller
 public class QuestionController {
     private final QuestionService questionService;
     
-    @GetMapping("/question/list")
+    @GetMapping("/list")
 //    @ResponseBody
     public String list(Model model) {               // 매개 변수 Model이 객체를 자동 생성
         List<Question> questionList = this.questionService.getList();
@@ -25,7 +27,7 @@ public class QuestionController {
         return "question_list";
     }
 
-    @GetMapping(value = "/question/detail/{id}")
+    @GetMapping(value = "/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id) {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
